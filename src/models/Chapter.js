1bed82ee6833a69be289f05d3259819ed65371e0
Fabian Romero <chapulinozeta@gmail.com>
2023-07-25 16:13:41 -0400

    Added src folder and models and a db file in it

---
+++ src/models/Chapter.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  sequelize.define(
+    'chapter',
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4, // Or DataTypes.UUIDV1
+        primaryKey: true,
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          notEmpty: true,
+        },
+      },
+      url: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      isActive: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+      },
+    },
+    {
+      timestamps: true,
+      createdAt: 'uploadedAt',
+      updatedAt: false,
+    }
+  );
+};
