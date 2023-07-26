3886d00609f0627609c69b0aa9d9a5d61b140eed
Fabian Romero <chapulinozeta@gmail.com>
2023-07-26 17:38:57 -0400

    Added server initialiazer

--- index.js
+++ index.js
@@ -19,10 +19,11 @@
 //     ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 const { conn } = require('./src/db');
+const server = require('./src/app.js');
 
 // Syncing all the models at once.
-conn.sync({ force: true }); /* .then(() => {
-  server.listen(PORT, () => {
+conn.sync({ force: true }).then(() => {
+  server.listen(3001, () => {
     console.log(`%s listening at 3001`); // eslint-disable-line no-console
   });
-}); */
+});
